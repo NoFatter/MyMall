18cdd98332d2bf0c8237d99b2225855fa584e58f
NoFatter <54098397+NoFatter@users.noreply.github.com>
2022-09-05 15:52:00 +0800

    member Nacos配置

--- mall-member/src/main/java/com/mall/member/MemberApplication.java
+++ mall-member/src/main/java/com/mall/member/MemberApplication.java
@@ -2,6 +2,7 @@ package com.mall.member;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Project name:MyMall
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * Create time:2022/7/14 20:32
  **/
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MemberApplication {
     public static void main(String[] args) {
         SpringApplication.run(MemberApplication.class,args);
