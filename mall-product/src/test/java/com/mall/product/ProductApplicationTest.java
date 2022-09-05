9f717d4f35e1376fa5d4a46773e2d73e5258c419
NoFatter <54098397+NoFatter@users.noreply.github.com>
2022-09-05 15:11:19 +0800

    test

--- mall-product/src/test/java/com/mall/product/ProductApplicationTest.java
+++ mall-product/src/test/java/com/mall/product/ProductApplicationTest.java
@@ -1,5 +1,7 @@
 package com.mall.product;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.mall.product.dto.BrandDTO;
 import com.mall.product.entity.BrandEntity;
 import com.mall.product.service.BrandService;
 import org.junit.Test;
@@ -7,6 +9,11 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 @RunWith(SpringRunner.class)
@@ -18,14 +25,26 @@ public class ProductApplicationTest {
     public void contextLoads(){
         BrandEntity brandEntity = new BrandEntity();
         /**
-            
+         * 按id查找
          */
+        brandEntity = brandService.selectById(2L);
+        System.out.println(brandEntity);
+//        /**
+//         * list查询dto
+//         */
+//        Map<String,Object> mapper = new HashMap<>();
+//
+//        mapper.put(brand_id,2);
+//        List<BrandDTO> list = brandService.list(mapper);
+//        for(BrandDTO brandDTO:list){
+//            System.out.println(brandDTO);
+//        }
         /**
          * 修改
          */
-        brandEntity.setBrandId(3L);
-        brandEntity.setDescript("爱国");
-        brandService.updateById(brandEntity);
+//        brandEntity.setBrandId(3L);
+//        brandEntity.setDescript("爱国");
+//        brandService.updateById(brandEntity);
         /**
          *  插入
          */
