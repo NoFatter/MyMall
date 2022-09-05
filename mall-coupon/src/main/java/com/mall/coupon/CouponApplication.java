7f098348d3fa921df71915fb00000b14a16a391b
NoFatter <54098397+NoFatter@users.noreply.github.com>
2022-09-05 15:43:16 +0800

    coupon Nacos配置

--- mall-coupon/src/main/java/com/mall/coupon/CouponApplication.java
+++ mall-coupon/src/main/java/com/mall/coupon/CouponApplication.java
@@ -2,12 +2,14 @@ package com.mall.coupon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Project name:MyMall
  * Author: NoFat
  * Create time:2022/7/14 20:26
  **/
+@EnableDiscoveryClient
 @SpringBootApplication
 public class CouponApplication {
     public static void main(String[] args) {
